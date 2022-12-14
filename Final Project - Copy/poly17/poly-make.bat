f17463c043da4915d6795f652bbd4c0afdbc0067
samjohnson1357 <63517427+samjohnson1357@users.noreply.github.com>
2022-12-14 15:19:19 -0700

    Delete Final Project - Copy directory

--- Final Project - Copy/poly17/poly-make.bat
+++
@@ -1,2 +0,0 @@
-polyglot make-book -pgn pgn\kasparov.pgn -bin books\kasparov.bin -max-ply 20 -min-game 1
-exit
--- Final Project - Copy/poly17/poly-merge.bat
+++
@@ -1,2 +0,0 @@
-polyglot merge-book -in1 books\prodeo.bin -in2 books\polyglot.bin -out books\book.bin
-exit
